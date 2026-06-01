9a827caef63b4124b62a74b3da729012f8ba24bc
jdhruv506-cmd <jdhruv506@gmail.com>
2026-06-01 14:15:04 +0530

    Add files via upload

---
+++ NameAge.java
@@ -0,0 +1,8 @@
+public class NameAge {
+    public static void main(String[] args) {
+        String name = "Dhruv"
+        int age = 21;
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
