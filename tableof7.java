ea3c21fb7a2f2be738ad5adfdb12a05fe50e3dc1
jdhruv506-cmd <jdhruv506@gmail.com>
2026-06-01 19:36:04 +0530

    Add files via upload

---
+++ tableof7.java
@@ -0,0 +1,10 @@
+public class tableof7 {
+    public static void main(String[] args) {
+        int number = 7;
+        System.out.println("Table of " + number + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+    
+}
